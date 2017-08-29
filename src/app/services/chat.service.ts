92e18650c1356326986fdba2cd469f853e3c230f
aurimas-darguzis <aurimas@darguzis.com>
2017-08-29 22:06:20 +0100

    building chat message model and connecting to service

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabaseModule, FirebaseListObservable  } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import { AuthService } from '../services/auth.service';
+import * as firebase from 'firebase/app';
+
+import { ChatMessage } from '../models/chat-message.model';
+
 
 @Injectable()
 export class ChatService {
 
-  constructor() { }
+  constructor(private af: AngularFireDatabaseModule) { }
+
+  sendMessage (message) {
+    // this.af.post(this.url, message);
+    return;
+  }
 
 }
