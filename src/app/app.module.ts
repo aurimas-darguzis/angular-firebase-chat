63d304bb1902dcf939b15c211ba7e0ff98a71365
aurimas-darguzis <aurimas@darguzis.com>
2017-08-29 19:39:48 +0100

    created components for application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,28 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ChatFormComponent } from './chat-form/chat-form.component';
+import { ChatRoomComponent } from './chat-room/chat-room.component';
+import { FeedComponent } from './feed/feed.component';
+import { MessageComponent } from './message/message.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserItemComponent } from './user-item/user-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatFormComponent,
+    ChatRoomComponent,
+    FeedComponent,
+    MessageComponent,
+    LoginFormComponent,
+    SignupFormComponent,
+    NavbarComponent,
+    UserListComponent,
+    UserItemComponent
   ],
   imports: [
     BrowserModule,
