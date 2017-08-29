a693beacfe20d48d817cdccb9aed9d5a2b572a9d
aurimas-darguzis <aurimas@darguzis.com>
2017-08-29 20:57:04 +0100

    imported modules for app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
 import { ChatFormComponent } from './chat-form/chat-form.component';
@@ -12,7 +16,13 @@ import { LoginFormComponent } from './login-form/login-form.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { UserListComponent } from './user-list/user-list.component';
-import { UserItemComponent } from './user-item/user-item.component';
+import { UserItemComponent } from './user-item/user-item.component'; 
+
+import { ChatService } from './services/chat.service';
+import { AuthService } from './services/auth.service';
+
+import { appRoutes } from '../routes';
+import { environment } from './../environments/environment';
 
 @NgModule({
   declarations: [
@@ -30,9 +40,17 @@ import { UserItemComponent } from './user-item/user-item.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    RouterModule.forRoot(appRoutes),
+    HttpModule,
+    AngularFireModule,
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebase),
   ],
-  providers: [],
+  providers: [
+    ChatService,
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
