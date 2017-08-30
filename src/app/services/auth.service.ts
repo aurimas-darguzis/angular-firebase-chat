ad758f02051024aacc4455772d40e3285a2b0402
aurimas-darguzis <aurimas@darguzis.com>
2017-08-30 22:41:54 +0100

    authState have new methods

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,64 @@
+import { User } from './../models/user.model';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import * as firebase from 'firebase/app';
 
 @Injectable()
 export class AuthService {
+  private user: Observable<firebase.User>;
+  private authState: any;
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth,
+    private db: AngularFireDatabase,
+    private router: Router) {
+      this.user  = afAuth.authState;
+    }
+
+  get currentUserId(): string {
+    return this.authState !== null ? this.authState.uid : '';
+  }
+
+  login(email: string, password: string) {
+    return this.afAuth.auth.signInWithEmailAndPassword(email, password)
+      .then((resolve) => {
+        const status = 'online';
+        this.setUserStatus(status);
+        this.router.navigate(['chat']);
+      })
+  }
+
+  signUp(email: string, password: string, displayName: string) {
+    return this.afAuth.auth.createUserWithEmailAndPassword(email, password)
+            .then((user) => {
+              this.authState = user;
+              const status = 'online';
+              this.setUserData(email, displayName, status);
+            }).catch(error => console.error(error));
+    }
+
+  setUserData(email: string, displayName: string, status: string): void {
+    const path = `users/${this.currentUserId}`;
+    const data = {
+      email,
+      displayName,
+      status
+    };
+
+    this.db.object(path).update(data)
+      .catch(error => console.error(error));
+  }
+
+  setUserStatus(status: string): void {
+    const path = `users/${this.currentUserId}`;
+    const data = {
+      status
+    };
+
+    this.db.object(path).update(data)
+      .catch(error => console.error(error));
+  }
 
 }
